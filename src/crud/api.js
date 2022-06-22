0317f85b3830b3f1abef94205d39cd57e2276913
nelson3design <mandeart2@gmail.com>
2022-06-22 15:31:26 -0300

    final-22

--- src/crud/api.js
+++ src/crud/api.js
@@ -1,30 +1,30 @@
 
  import axios from 'axios'
 export const getNames= async  ()=>{
-    return  await axios.get('https://nelsoncrud.herokuapp.com/data')
+    return  await axios.get('https://nelsondb.herokuapp.com/data/')
 }
 
 
 export const createName= async  (nome,email,contato)=>{
-    return  await axios.post('https://nelsoncrud.herokuapp.com/data',nome,email,contato)
+    return  await axios.post('https://nelsondb.herokuapp.com/data/',nome,email,contato)
 }
 
 export const removeName= async  (id)=>{
-    return  await axios.delete('https://nelsoncrud.herokuapp.com/data/'+ id)
+    return  await axios.delete('https://nelsondb.herokuapp.com/data/'+ id)
 }
 
 
 export const getName= async  (id)=>{
-    return  await axios.get('https://nelsoncrud.herokuapp.com/data/'+ id)
+    return  await axios.get('https://nelsondb.herokuapp.com/data/'+ id)
 }
 
 export const updateName= async  (id,nome,email,contato)=>{
-    return  await axios.put('https://nelsoncrud.herokuapp.com/data/'+ id,nome,email,contato)
+    return  await axios.put('https://nelsondb.herokuapp.com/data/'+ id,nome,email,contato)
 }
 
 
 export const searchName= async  (nome)=>{
-    return  await axios.get('https://nelsoncrud.herokuapp.com/data?q='+ nome)
+    return  await axios.get('https://nelsondb.herokuapp.com/data?q='+ nome)
 
 }
 
--- src/crud/index.js
+++ src/crud/index.js
@@ -32,7 +32,7 @@ const Crud =() =>{
       const handleSearch= async (e)=>{
         e.preventDefault()
          return await axios 
-         .get(`https://nelsoncrud.herokuapp.com/data?q=${value}`)
+         .get(`https://nelsondb.herokuapp.com/data?q=${value}`)
          .then((response)=>{
           setUsers(response.data)
           
